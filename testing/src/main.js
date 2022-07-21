92da745cbe3a5d691fb3c7a0daff545273874921
Wathanyu Suliamma <tan_sanook02@hotmail.com>
2022-07-22 04:00:33 +0700

    4.12

---
+++ testing/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import rounter from "./rounter";
+import store from "./store";
+createApp(App).mount("#app");
+let x = "helxysx";
+console.log(x);
